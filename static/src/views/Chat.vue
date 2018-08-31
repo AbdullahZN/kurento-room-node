fcdbcb7a0ea44e07932434f4f1a5468d77aa6c0a
Abdullah Uzun <auzun@adobe.com>
2018-08-31 22:19:26 +0200

    add autoplay

--- static/src/views/Chat.vue
+++ static/src/views/Chat.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="video-list">
     <template v-for="participant in $store.state.participants">
-      <video :src="participant.src"></video>
+      <video :key="participant.id" autoplay :src="participant.src"></video>
     </template>
   </div>
 </template>
